da49a47bb4f51e67100822d4f9875ccf8df17eb0
Benoit Lord <benoit.lord@canada.ca>
2020-07-30 14:41:50 -0400

    First commit from TFS cs3598

---
+++ settings/settings-general.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+define({
+
+
+	/*------------------ Course info -----------------*/
+	courseLegacyCode:"K102",
+	seriesTitle_en: "K102",
+	seriesTitle_fr: "K102",
+	courseTitle_en: "Métis in Canada",
+	courseTitle_fr: "Les Métis au Canada",
+
+	showGlossary: true,
+	triggerCompletionWhenAllPagesViewed: true,
+
+	"googleAnalyticsID": "UA-101907595-23"
+});
